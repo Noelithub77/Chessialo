5f0501da026eda033b3871622b74e629ac7ecb0e
Noelithub77 <noelmcv7@gmail.com>
2025-01-04 11:06:33 +0530

    First working model via the button thing

--- board.gd
+++ board.gd
@@ -2,7 +2,7 @@ extends GridContainer
 @export var square_scene : PackedScene
 signal square_clicked(grid_pos: Vector2)
 
-var grid = []
+@export var grid = []
 var square_size = 64  
 var board_size = 8
 var occupied_squares = {}
@@ -12,18 +12,20 @@ var occupied_by_piece = {}
 func _ready():
 	create_grid()
 	print("board's _ready")
+	#_on_square_pressed(Vector2(6,6))
 
 func create_grid():
 	for row in range(board_size):
 		for col in range(board_size):
 			var square_instance = square_scene.instantiate()
+			square_instance.name = str(Vector2(col,row)) 
 			var color = Color.WHITE if (row + col) % 2 == 0 else Color.DARK_GRAY
 			# Update to reference ColorRect directly
 			var color_rect = square_instance.get_node("ColorRect")
 			color_rect.color = color
 			# Get button as child of ColorRect
 			var button = color_rect.get_node("Button")
-			button.pressed.connect(_on_square_pressed.bind(Vector2(col, row)))
+			button.connect("pressed", Callable(self, "on_square_pressed").bind(Vector2(col, row)))
 			
 			# Set position for each square
 			square_instance.position = Vector2(col * square_size, row * square_size)
@@ -33,7 +35,7 @@ func create_grid():
 			occupied_squares[Vector2(col,row)] = false
 			occupied_by_piece[Vector2(col,row)] = "none"
 
-func _on_square_pressed(grid_pos: Vector2):
+func on_square_pressed(grid_pos: Vector2):
 	square_clicked.emit(grid_pos)
 	print("Square clicked at: ", grid_pos)
 
@@ -43,9 +45,4 @@ func get_square(grid_pos: Vector2):
 		return grid[index]
 	return null
 
-	# func is_square_pressed(grid_pos: Vector2) -> bool:
-	# 	var square = get_square(grid_pos)
-	# 	if square:
-	# 		var button = square.get_node("Button")
-	# 		return button.is_pressed()
-	# 	return false
+	
